82a5f1b56172be4f85a6eaa7d04247e28e0b6286
aero <aerooneQ@yandex.ru>
2023-11-19 17:23:26 +0300

    cargo fmt

--- src/rust/bxes/src/single_file_bxes_reader.rs
+++ src/rust/bxes/src/single_file_bxes_reader.rs
@@ -1,9 +1,9 @@
-use std::rc::Rc;
-use std::string::FromUtf8Error;
-use binary_rw::{BinaryReader, Endian, FileStream, SeekStream};
-use num_derive::FromPrimitive;
 use crate::num::FromPrimitive;
 use crate::type_ids;
+use binary_rw::{BinaryReader, Endian, FileStream, SeekStream};
+use num_derive::FromPrimitive;
+use std::rc::Rc;
+use std::string::FromUtf8Error;
 
 #[derive(Clone, Debug)]
 pub enum BxesValue {
@@ -17,13 +17,13 @@ pub enum BxesValue {
     Bool(bool),
     Timestamp(i64),
     BrafLifecycle(BrafLifecycle),
-    StandardLifecycle(StandardLifecycle)
+    StandardLifecycle(StandardLifecycle),
 }
 
 #[derive(Clone, Debug)]
 pub enum Lifecycle {
     Braf(BrafLifecycle),
-    Standard(StandardLifecycle)
+    Standard(StandardLifecycle),
 }
 
 #[derive(FromPrimitive, Clone, Debug)]
@@ -72,13 +72,13 @@ pub enum StandardLifecycle {
 pub struct BxesEventLog {
     pub version: u32,
     pub metadata: Option<Vec<(Rc<Box<String>>, BxesValue)>>,
-    pub variants: Vec<BxesTraceVariant>
+    pub variants: Vec<BxesTraceVariant>,
 }
 
 #[derive(Debug)]
 pub struct BxesTraceVariant {
     pub traces_count: u32,
-    pub events: Vec<BxesEvent>
+    pub events: Vec<BxesEvent>,
 }
 
 #[derive(Debug)]
@@ -86,7 +86,7 @@ pub struct BxesEvent {
     pub name: Rc<Box<String>>,
     pub timestamp: i64,
     pub lifecycle: Lifecycle,
-    pub attributes: Option<Vec<(Rc<Box<String>>, BxesValue)>>
+    pub attributes: Option<Vec<(Rc<Box<String>>, BxesValue)>>,
 }
 
 #[derive(Debug)]
@@ -100,21 +100,18 @@ pub enum BxesReadError {
     FailedToIndexKeyValue(usize),
     NameOfEventIsNotAString,
     LifecycleOfEventOutOfRange,
-    EventAttributeKeyIsNotAString
+    EventAttributeKeyIsNotAString,
 }
 
 #[derive(Debug)]
 pub struct FailedToReadValueError {
     pub offset: usize,
-    pub message: String
+    pub message: String,
 }
 
 impl FailedToReadValueError {
     pub fn new(offset: usize, message: String) -> Self {
-        Self {
-            offset,
-            message
-        }
+        Self { offset, message }
     }
 }
 
@@ -128,10 +125,18 @@ pub fn read_bxes(path: &str) -> Result<BxesEventLog, BxesReadError> {
     let metadata = try_read_event_log_metadata(&mut reader, &values, &kv_pairs)?;
     let variants = try_read_traces_variants(&mut reader, &values, &kv_pairs)?;
 
-    Ok(BxesEventLog { version, metadata, variants })
+    Ok(BxesEventLog {
+        version,
+        metadata,
+        variants,
+    })
 }
 
-fn try_read_event_log_metadata(reader: &mut BinaryReader, values: &Vec<BxesValue>, kv_pairs: &Vec<(u32, u32)>) -> Result<Option<Vec<(Rc<Box<String>>, BxesValue)>>, BxesReadError> {
+fn try_read_event_log_metadata(
+    reader: &mut BinaryReader,
+    values: &Vec<BxesValue>,
+    kv_pairs: &Vec<(u32, u32)>,
+) -> Result<Option<Vec<(Rc<Box<String>>, BxesValue)>>, BxesReadError> {
     let metadata_count = try_read_u32(reader)?;
     if metadata_count == 0 {
         Ok(None)
@@ -146,7 +151,11 @@ fn try_read_event_log_metadata(reader: &mut BinaryReader, values: &Vec<BxesValue
     }
 }
 
-fn try_read_traces_variants(reader: &mut BinaryReader, values: &Vec<BxesValue>, kv_pairs: &Vec<(u32, u32)>) -> Result<Vec<BxesTraceVariant>, BxesReadError> {
+fn try_read_traces_variants(
+    reader: &mut BinaryReader,
+    values: &Vec<BxesValue>,
+    kv_pairs: &Vec<(u32, u32)>,
+) -> Result<Vec<BxesTraceVariant>, BxesReadError> {
     let mut variants = vec![];
     let variant_count = try_read_u32(reader)?;
 
@@ -157,7 +166,11 @@ fn try_read_traces_variants(reader: &mut BinaryReader, values: &Vec<BxesValue>, 
     Ok(variants)
 }
 
-fn try_read_trace_variant(reader: &mut BinaryReader, values: &Vec<BxesValue>, kv_pairs: &Vec<(u32, u32)>) -> Result<BxesTraceVariant, BxesReadError> {
+fn try_read_trace_variant(
+    reader: &mut BinaryReader,
+    values: &Vec<BxesValue>,
+    kv_pairs: &Vec<(u32, u32)>,
+) -> Result<BxesTraceVariant, BxesReadError> {
     let traces_count = try_read_u32(reader)?;
     let events_count = try_read_u32(reader)?;
     let mut events = vec![];
@@ -168,11 +181,15 @@ fn try_read_trace_variant(reader: &mut BinaryReader, values: &Vec<BxesValue>, kv
 
     Ok(BxesTraceVariant {
         traces_count,
-        events
+        events,
     })
 }
 
-fn try_read_event(reader: &mut BinaryReader, values: &Vec<BxesValue>, kv_pairs: &Vec<(u32, u32)>) -> Result<BxesEvent, BxesReadError> {
+fn try_read_event(
+    reader: &mut BinaryReader,
+    values: &Vec<BxesValue>,
+    kv_pairs: &Vec<(u32, u32)>,
+) -> Result<BxesEvent, BxesReadError> {
     let name_index = try_read_u32(reader)? as usize;
     let name = values.get(name_index);
 
@@ -182,25 +199,29 @@ fn try_read_event(reader: &mut BinaryReader, values: &Vec<BxesValue>, kv_pairs: 
 
     let name_string = match name.unwrap() {
         BxesValue::String(string) => string,
-        _ => return Err(BxesReadError::NameOfEventIsNotAString)
+        _ => return Err(BxesReadError::NameOfEventIsNotAString),
     };
 
     let timestamp = try_read_i64(reader)?;
     let lifecycle = match try_read_bxes_value(reader)? {
         BxesValue::StandardLifecycle(lifecycle) => Lifecycle::Standard(lifecycle),
         BxesValue::BrafLifecycle(lifecycle) => Lifecycle::Braf(lifecycle),
-        _ => return Err(BxesReadError::LifecycleOfEventOutOfRange)
+        _ => return Err(BxesReadError::LifecycleOfEventOutOfRange),
     };
 
     Ok(BxesEvent {
         name: name_string.clone(),
         timestamp,
         lifecycle,
-        attributes: try_read_event_attributes(reader, values, kv_pairs)?
+        attributes: try_read_event_attributes(reader, values, kv_pairs)?,
     })
 }
 
-fn try_read_event_attributes(reader: &mut BinaryReader, values: &Vec<BxesValue>, kv_pairs: &Vec<(u32, u32)>) -> Result<Option<Vec<(Rc<Box<String>>, BxesValue)>>, BxesReadError> {
+fn try_read_event_attributes(
+    reader: &mut BinaryReader,
+    values: &Vec<BxesValue>,
+    kv_pairs: &Vec<(u32, u32)>,
+) -> Result<Option<Vec<(Rc<Box<String>>, BxesValue)>>, BxesReadError> {
     let attributes_count = try_read_u32(reader)?;
     if attributes_count == 0 {
         Ok(None)
@@ -215,28 +236,32 @@ fn try_read_event_attributes(reader: &mut BinaryReader, values: &Vec<BxesValue>,
     }
 }
 
-fn try_read_kv_pair(reader: &mut BinaryReader, values: &Vec<BxesValue>, kv_pairs: &Vec<(u32, u32)>) -> Result<(Rc<Box<String>>, BxesValue), BxesReadError> {
+fn try_read_kv_pair(
+    reader: &mut BinaryReader,
+    values: &Vec<BxesValue>,
+    kv_pairs: &Vec<(u32, u32)>,
+) -> Result<(Rc<Box<String>>, BxesValue), BxesReadError> {
     let kv_index = try_read_u32(reader)? as usize;
     let kv_pair = match kv_pairs.get(kv_index) {
         None => return Err(BxesReadError::FailedToIndexKeyValue(kv_index)),
-        Some(pair) => pair
+        Some(pair) => pair,
     };
 
     let key_index = kv_pair.0 as usize;
     let key = match values.get(key_index) {
         None => return Err(BxesReadError::FailedToIndexValue(key_index)),
-        Some(value) => value
+        Some(value) => value,
     };
 
     let key_string = match key {
         BxesValue::String(string) => string.clone(),
-        _ => return Err(BxesReadError::EventAttributeKeyIsNotAString)
+        _ => return Err(BxesReadError::EventAttributeKeyIsNotAString),
     };
 
     let value_index = kv_pair.1 as usize;
     let value = match values.get(value_index) {
         None => return Err(BxesReadError::FailedToIndexValue(value_index)),
-        Some(value) => value
+        Some(value) => value,
     };
 
     Ok((key_string, value.clone()))
@@ -275,11 +300,15 @@ fn try_read_bxes_value(reader: &mut BinaryReader) -> Result<BxesValue, BxesReadE
         type_ids::F_32 => Ok(BxesValue::Float32(try_read_f32(reader)?)),
         type_ids::F_64 => Ok(BxesValue::Float64(try_read_f64(reader)?)),
         type_ids::BOOL => Ok(BxesValue::Bool(try_read_bool(reader)?)),
-        type_ids::STRING => Ok(BxesValue::String(Rc::new(Box::new(try_read_string(reader)?)))),
+        type_ids::STRING => Ok(BxesValue::String(Rc::new(Box::new(try_read_string(
+            reader,
+        )?)))),
         type_ids::TIMESTAMP => Ok(BxesValue::Timestamp(try_read_i64(reader)?)),
         type_ids::BRAF_LIFECYCLE => Ok(BxesValue::BrafLifecycle(try_read_braf_lifecycle(reader)?)),
-        type_ids::STANDARD_LIFECYCLE => Ok(BxesValue::StandardLifecycle(try_read_standard_lifecycle(reader)?)),
-        _ => Err(BxesReadError::FailedToParseTypeId(type_id))
+        type_ids::STANDARD_LIFECYCLE => Ok(BxesValue::StandardLifecycle(
+            try_read_standard_lifecycle(reader)?,
+        )),
+        _ => Err(BxesReadError::FailedToParseTypeId(type_id)),
     }
 }
 
@@ -321,7 +350,7 @@ fn try_read_string(reader: &mut BinaryReader) -> Result<String, BxesReadError> {
 
     match String::from_utf8(bytes) {
         Ok(string) => Ok(string),
-        Err(err) => Err(BxesReadError::FailedToCreateUtf8String(err))
+        Err(err) => Err(BxesReadError::FailedToCreateUtf8String(err)),
     }
 }
 
@@ -329,7 +358,9 @@ fn try_read_bytes(reader: &mut BinaryReader, length: usize) -> Result<Vec<u8>, B
     let offset = try_tell_pos(reader)?;
     match reader.read_bytes(length) {
         Ok(bytes) => Ok(bytes),
-        Err(err) => Err(BxesReadError::FailedToReadValue(FailedToReadValueError::new(offset, err.to_string())))
+        Err(err) => Err(BxesReadError::FailedToReadValue(
+            FailedToReadValueError::new(offset, err.to_string()),
+        )),
     }
 }
 
@@ -341,31 +372,40 @@ fn try_read_enum<T: FromPrimitive>(reader: &mut BinaryReader) -> Result<T, BxesR
     let offset = try_tell_pos(reader)?;
     match reader.read_u8() {
         Ok(byte) => Ok(T::from_u8(byte).unwrap()),
-        Err(err) => Err(BxesReadError::FailedToReadValue(FailedToReadValueError::new(offset, err.to_string())))
+        Err(err) => Err(BxesReadError::FailedToReadValue(
+            FailedToReadValueError::new(offset, err.to_string()),
+        )),
     }
 }
 
-fn try_read_standard_lifecycle(reader: &mut BinaryReader) -> Result<StandardLifecycle, BxesReadError> {
+fn try_read_standard_lifecycle(
+    reader: &mut BinaryReader,
+) -> Result<StandardLifecycle, BxesReadError> {
     try_read_enum::<StandardLifecycle>(reader)
 }
 
 fn try_open_file_stream(path: &str) -> Result<FileStream, BxesReadError> {
     match FileStream::open(path) {
         Ok(fs) => Ok(fs),
-        Err(err) => Err(BxesReadError::FailedToOpenFile(err.to_string()))
+        Err(err) => Err(BxesReadError::FailedToOpenFile(err.to_string())),
     }
 }
 
-fn try_read_primitive_value<T>(reader_pos: usize, mut read_func: impl FnMut() -> binary_rw::Result<T>) -> Result<T, BxesReadError> {
+fn try_read_primitive_value<T>(
+    reader_pos: usize,
+    mut read_func: impl FnMut() -> binary_rw::Result<T>,
+) -> Result<T, BxesReadError> {
     match read_func() {
         Ok(value) => Ok(value),
-        Err(err) => Err(BxesReadError::FailedToReadValue(FailedToReadValueError::new(reader_pos, err.to_string())))
+        Err(err) => Err(BxesReadError::FailedToReadValue(
+            FailedToReadValueError::new(reader_pos, err.to_string()),
+        )),
     }
 }
 
 fn try_tell_pos(reader: &mut BinaryReader) -> Result<usize, BxesReadError> {
     match reader.tell() {
         Ok(pos) => Ok(pos),
-        Err(err) => Err(BxesReadError::FailedToReadPos(err.to_string()))
+        Err(err) => Err(BxesReadError::FailedToReadPos(err.to_string())),
     }
 }
