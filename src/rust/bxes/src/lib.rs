127ee77cec7864ecc1cf4666af0ad9557e31c097
aero <aerooneQ@yandex.ru>
2023-11-19 20:27:33 +0300

    read from multiple files

--- src/rust/bxes/src/lib.rs
+++ src/rust/bxes/src/lib.rs
@@ -3,6 +3,7 @@ extern crate num;
 #[macro_use]
 extern crate num_derive;
 
+pub mod constants;
 pub mod models;
 pub mod read;
 pub mod type_ids;
--- src/rust/bxes/src/read/single_file_bxes_reader.rs
+++ src/rust/bxes/src/read/single_file_bxes_reader.rs
@@ -1,8 +1,7 @@
-use binary_rw::{BinaryReader, Endian};
-
-use crate::models::*;
-
 use super::{errors::BxesReadError, read_utils::*};
+use crate::{constants::*, models::*};
+use binary_rw::{BinaryReader, Endian};
+use std::path::Path;
 
 pub fn read_bxes(path: &str) -> Result<BxesEventLog, BxesReadError> {
     let mut stream = try_open_file_stream(path)?;
@@ -19,4 +18,50 @@ pub fn read_bxes(path: &str) -> Result<BxesEventLog, BxesReadError> {
         metadata,
         variants,
     })
+}
+
+pub fn read_bxes_multiple_files(directory_path: &str) -> Result<BxesEventLog, BxesReadError> {
+    let values = read_file(directory_path, VALUES_FILE_NAME, |reader| {
+        try_read_values(reader)
+    })?;
+
+    let kv_pairs = read_file(directory_path, KEY_VALUES_FILE_NAME, |reader| {
+        try_read_key_values(reader)
+    })?;
+
+    let metadata = read_file(directory_path, METADATA_FILE_NAME, |reader| {
+        try_read_event_log_metadata(reader, &values, &kv_pairs)
+    })?;
+
+    let variants = read_file(directory_path, VARIANTS_FILE_NAME, |reader| {
+        try_read_traces_variants(reader, &values, &kv_pairs)
+    })?;
+
+    Ok(BxesEventLog {
+        version: 0,
+        metadata,
+        variants,
+    })
+}
+
+fn read_file<T>(
+    directory_path: &str,
+    file_name: &str,
+    reader_func: impl FnMut(&mut BinaryReader) -> Result<T, BxesReadError>,
+) -> Result<T, BxesReadError> {
+    let directory_path = Path::new(directory_path);
+    let file_path = directory_path.join(file_name);
+    let file_path = file_path.to_str().unwrap();
+
+    execute_with_reader(file_path, reader_func)
+}
+
+fn execute_with_reader<T>(
+    path: &str,
+    mut reader_func: impl FnMut(&mut BinaryReader) -> Result<T, BxesReadError>,
+) -> Result<T, BxesReadError> {
+    let mut stream = try_open_file_stream(path)?;
+    let mut reader = BinaryReader::new(&mut stream, Endian::Little);
+
+    reader_func(&mut reader)
 }
