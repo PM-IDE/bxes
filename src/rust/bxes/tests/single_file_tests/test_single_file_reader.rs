ccbb5eba12ed19cdc9fd68914b04e28e2bf3c6f5
aero <aerooneQ@yandex.ru>
2023-11-25 14:13:19 +0300

    Add multiple file tests

--- src/rust/bxes/tests/single_file_tests/test_single_file_reader.rs
+++ src/rust/bxes/tests/single_file_tests/test_single_file_reader.rs
@@ -3,14 +3,14 @@ use std::path::Path;
 use bxes::{read::single_file_bxes_reader::read_bxes, writer::single_file_bxes_writer::write_bxes};
 use tempfile::TempDir;
 
-use crate::test_core::random_log::{self, generate_random_log};
+use crate::test_core::random_log::generate_random_log;
 
 #[test]
 pub fn test_single_file_read_write() {
     let log = generate_random_log();
     let temp_dir = TempDir::new().unwrap();
     let log_file_name = "log.bxes";
-    let temp_dir = temp_dir.path().as_os_str().to_str().unwrap(); 
+    let temp_dir = temp_dir.path().as_os_str().to_str().unwrap();
     let log_save_path = Path::new(temp_dir).join(log_file_name);
 
     write_bxes(log_save_path.to_str().unwrap(), &log).ok();
