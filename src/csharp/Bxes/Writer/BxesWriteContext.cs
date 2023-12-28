9b492070071b8cf3d707e9b4eba8fcf8a34f7255
aero <aerooneQ@yandex.ru>
2023-12-28 15:34:29 +0300

    Stupid code fix

--- src/csharp/Bxes/Writer/BxesWriteContext.cs
+++ src/csharp/Bxes/Writer/BxesWriteContext.cs
@@ -5,20 +5,28 @@ namespace Bxes.Writer;
 
 public record AttributeKeyValue(BxesStringValue Key, BxesValue Value);
 
-public readonly struct BxesWriteContext(BinaryWriter binaryWriter)
+public readonly struct BxesWriteContext
 {
-  public BinaryWriter Writer { get; } = binaryWriter;
-  public Dictionary<BxesValue, uint> ValuesIndices { get; } = new();
-  public Dictionary<AttributeKeyValue, uint> KeyValueIndices { get; } = new();
+  public BinaryWriter Writer { get; }
+  public Dictionary<BxesValue, uint> ValuesIndices { get; }
+  public Dictionary<AttributeKeyValue, uint> KeyValueIndices { get; }
 
 
   private BxesWriteContext(
     BinaryWriter writer,
     Dictionary<BxesValue, uint> valuesIndices,
-    Dictionary<AttributeKeyValue, uint> keyValueIndices) : this(writer)
+    Dictionary<AttributeKeyValue, uint> keyValueIndices)
   {
+    Writer = writer;
     ValuesIndices = valuesIndices;
     KeyValueIndices = keyValueIndices;
+  }
+
+  public BxesWriteContext(BinaryWriter binaryWriter)
+  {
+    Writer = binaryWriter;
+    ValuesIndices = new Dictionary<BxesValue, uint>();
+    KeyValueIndices = new Dictionary<AttributeKeyValue, uint>();
   }
 
 
