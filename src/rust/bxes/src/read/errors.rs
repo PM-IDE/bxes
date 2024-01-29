556b5b0aef20105fd9d3c8bea930d2c625c9451d
aero <aerooneQ@yandex.ru>
2024-01-29 03:58:09 +0300

    Some ToStrings

--- src/rust/bxes/src/read/errors.rs
+++ src/rust/bxes/src/read/errors.rs
@@ -1,5 +1,4 @@
 use std::string::FromUtf8Error;
-
 use crate::models::BxesValue;
 
 #[derive(Debug)]
@@ -22,10 +21,39 @@ pub enum BxesReadError {
     Leb128ReadError(String),
 }
 
+impl ToString for BxesReadError {
+    fn to_string(&self) -> String {
+        match self {
+            BxesReadError::FailedToOpenFile(value) => format!("Failed to open file {}", value),
+            BxesReadError::FailedToReadValue(err) => format!("Failed to read value: {}", err.to_string()),
+            BxesReadError::FailedToReadPos(error_message) => format!("Failed to read pos from stream: {}", error_message),
+            BxesReadError::FailedToCreateUtf8String(err) => format!("Failed to create string: {}", err.to_string()),
+            BxesReadError::FailedToParseTypeId(type_id) => format!("Failed to parse type id: {}", type_id),
+            BxesReadError::FailedToIndexValue(index) => format!("Failed to find bxes value for index: {}", index),
+            BxesReadError::FailedToIndexKeyValue(index) => format!("Failed to find kv pair for index: {}", index),
+            BxesReadError::LifecycleOfEventOutOfRange => format!("LifecycleOfEventOutOfRange"),
+            BxesReadError::EventAttributeKeyIsNotAString => format!("EventAttributeKeyIsNotAString"),
+            BxesReadError::VersionsMismatchError(err) => err.to_string(),
+            BxesReadError::FailedToExtractArchive => format!("FailedToExtractArchive"),
+            BxesReadError::TooManyFilesInArchive => format!("TooManyFilesInArchive"),
+            BxesReadError::FailedToCreateTempDir => format!("FailedToCreateTempDir"),
+            BxesReadError::InvalidArchive(message) => format!("Invalid bxes archive: {}", message),
+            BxesReadError::ExpectedString(value) => format!("Expected string value, found: {:?}", value),
+            BxesReadError::Leb128ReadError(message) => format!("Failed to read LEB128 encoded value: {}", message)
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct FailedToReadValueError {
     pub offset: usize,
     pub message: String,
+}
+
+impl ToString for FailedToReadValueError {
+    fn to_string(&self) -> String {
+        format!("Failed to read value at offset {}, error: {}", self.offset, self.message)
+    }
 }
 
 impl FailedToReadValueError {
@@ -38,6 +66,12 @@ impl FailedToReadValueError {
 pub struct VersionsMismatchError {
     previous_version: u32,
     current_version: u32,
+}
+
+impl ToString for VersionsMismatchError {
+    fn to_string(&self) -> String {
+        format!("Versions mismatch: previous version: {}, current version: {}", self.previous_version, self.current_version)
+    }
 }
 
 impl VersionsMismatchError {
